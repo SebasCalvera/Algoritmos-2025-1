a1fce4b9e4d578f53742c864934972f7b40baf14
SamuelMancera <samumancardenas@gmail.com>
2025-05-03 22:58:03 -0500

    Update AdivinaNum.java

--- M3/AdivinaNum.java
+++ M3/AdivinaNum.java
@@ -3,7 +3,7 @@ package M3;
 import java.util.Scanner;
 
 public class AdivinaNum {
-    static int NumSecreto = 19;//int numeroSecreto = (int) (Math.random()*100)+1//
+    static int NumSecreto = 19;//int numeroSecreto = (int) (Math.random()*20)+1//
     public static void main(String[] args) {
         
         Instrucciones();
